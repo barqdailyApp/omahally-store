168faa9d4b1120e015f6aa0418e72baafb0f889c
imbeltagy <imbeltagy@gmail.com>
2026-06-08 19:48:12 +0300

    feat(copyrights): add created by Omahally

--- src/layouts/common/copyrights.tsx
+++ src/layouts/common/copyrights.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import { useTranslations } from "next-intl";
 
 import Stack from "@mui/material/Stack";
@@ -21,10 +22,32 @@ export default function Copyrights() {
           alignItems="center"
           justifyContent="space-between"
           py={1}
+          gap={1}
         >
           <Typography color="text.secondary" variant="caption">
             {t("copyright", { year: new Date().getFullYear() })}
           </Typography>
+
+          <Stack direction="row" alignItems="center" spacing={0.5}>
+            <Typography color="text.disabled" variant="caption">
+              {t("created_by")}
+            </Typography>
+            <Box
+              sx={{
+                width: { xs: 93, md: 124 },
+                height: { xs: 36, md: 48 },
+                position: "relative",
+              }}
+            >
+              <Image
+                src="/logo/logo-full-620-240.png"
+                fill
+                alt="Omahally"
+                style={{ objectFit: "contain" }}
+              />
+            </Box>
+          </Stack>
+
           <Stack direction="row" spacing={0.5}>
             {SOCIAL_LINKS.map((item) => (
               <IconButton
