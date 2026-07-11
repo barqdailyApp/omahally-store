0eb3367aac1e13e5f8268529b2a066c310118c3b
Ahmed <a.maghraby96@yahoo.com>
2026-07-11 05:16:40 +0300

    feat: add layout wrapper with container and stack for category pages

--- src/app/[locale]/(store-layout)/category/layout.tsx
+++ src/app/[locale]/(store-layout)/category/layout.tsx
@@ -4,7 +4,7 @@ import { Stack, Container } from "@mui/material";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <Stack spacing={2} minHeight="100%" component={Container}>
+    <Stack spacing={2} minHeight="100%" component={Container} sx={{ pt: 2.5 }}>
       {children}
     </Stack>
   );
